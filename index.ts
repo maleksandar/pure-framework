cd21eeee6e26bdfbd5edf04f4efe03789b7d7a1c
Aleksandar Milosavljević <alemilosav@gmail.com>
2021-08-24 17:21:11 +0200

    removing todo app files

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrap } from './core';
-import { todoList } from './ToDoListComponent';
-import { store } from './stores/todo.store';
-
-bootstrap(document.getElementById('app'), todoList, store);
